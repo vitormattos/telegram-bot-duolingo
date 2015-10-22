a798c40c037221c123deeca5c820983c757c92e1
Vitor Mattos <vitor.mattos@phprio.org>
2015-10-22 18:59:32 -0200

    teste

--- src/commands/RegisterCommand.php
+++ src/commands/RegisterCommand.php
@@ -1,17 +1,21 @@
 <?php
 namespace Commands;
+
 use Telegram\Bot\Commands\Command;
 use Telegram\Bot\Actions;
 use Base\DB;
 
 class RegisterCommand extends Command
 {
+
     /**
+     *
      * @var string Command Name
      */
     protected $name = "register";
 
     /**
+     *
      * @var string Command Description
      */
     protected $description = "Register your duolingo username in this group";
@@ -23,29 +27,25 @@ class RegisterCommand extends Command
     {
         // This will update the chat status to typing...
         $this->replyWithChatAction(Actions::TYPING);
-
-        if(!$arguments) {
+        
+        if (! $arguments) {
             $this->replyWithMessage('Enter your duolingo username, example:');
             $this->replyWithMessage('/register MyUsername');
         } else {
-            $profile = json_decode(file_get_contents('https://www.duolingo.com/users/'.$arguments));
-            if($profile) {
+            $profile = json_decode(file_get_contents('https://www.duolingo.com/users/' . $arguments));
+            if ($profile) {
                 $update = $this->telegram->getWebhookUpdates()->all();
-
+                
                 $db = DB::getInstance();
                 try {
-                    $db->perform(
-                        "INSERT INTO users (username, registered_by, created) VALUES (:username, :registered_by, :created)",
-                        [
-                            'username'      => $profile->username,
-                            'registered_by' => $update['message']['from']['id'],
-                            'created' => date('Y-m-d H:i:s', $update['message']['date'])
-                        ]
-                    );
-                    $this->replyWithMessage('Welcome '.($profile->fullname?:$profile->username).'!');
-                } catch(\Exception $e) {
-                    $this->replyWithMessage($e->getMessage())
-;                    $this->replyWithMessage(($profile->fullname?:$profile->username).' already registered.');
+                    $db->perform("INSERT INTO users (username, registered_by, created) VALUES (:username, :registered_by, :created)", [
+                        'username' => $profile->username,
+                        'registered_by' => $update['message']['from']['id'],
+                        'created' => date('Y-m-d H:i:s', $update['message']['date'])
+                    ]);
+                    $this->replyWithMessage('Welcome ' . ($profile->fullname ?  : $profile->username) . '!');
+                } catch (\Exception $e) {
+                    $this->replyWithMessage(($profile->fullname ?  : $profile->username) . ' already registered.');
                 }
             } else {
                 $this->replyWithMessage('Invalid username');
