1bab207192fa10509c459ec2775cd3ac69236bad
Vitor Mattos <vitor.mattos@phprio.org>
2015-10-22 15:13:34 -0200

    teste

--- src/commands/RegisterCommand.php
+++ src/commands/RegisterCommand.php
@@ -24,7 +24,7 @@ class RegisterCommand extends Command
         // This will update the chat status to typing...
         $this->replyWithChatAction(Actions::TYPING);
 
-        $this->replyWithMessage(print_r($this->telegram->getLastResponse()->getDecodedBody(), true));
+        $this->replyWithMessage(print_r($this->telegram->getWebhookUpdates(), true));
         return;
         if(!$arguments) {
             $this->replyWithMessage('Enter your duolingo username, example:');
