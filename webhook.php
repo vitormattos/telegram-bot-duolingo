b658aaea1654212b80c0018f84dae6f0b27cb6cb
Vitor Mattos <vitor.mattos@phprio.org>
2015-10-22 11:35:29 -0200

    melhorias ao registrar webhook

--- webhook.php
+++ webhook.php
@@ -1,8 +1,33 @@
 <?php
 use Telegram\Bot\Api;
+require 'vendor/autoload.php';
 require 'config.php';
 
-$telegram = new Api($config['token']);
+$url = $_GET['url'];
+if(filter_var('https://'.$url, FILTER_VALIDATE_URL) == false) {
+    echo 'Invalid url for get certificate';
+    die();
+}
+$g = stream_context_create (array("ssl" => array(
+    "capture_peer_cert" => true,
+    "verify_peer" => false,
+    "verify_peer_name" => false
+)));
+$r = stream_socket_client("ssl://{$url}:443", $errno, $errstr, 30,
+    STREAM_CLIENT_CONNECT, $g);
+if(!$r) {
+    echo 'Domain dont exists';
+    die();
+}
+$cont = stream_context_get_params($r);
+openssl_x509_export($cont["options"]["ssl"]["peer_certificate"], $certificate);
 
-$response = $telegram->setWebhook('https://telegram-bot-duolingo.appspot.com');
-var_dump($response);
+$telegram = new Api($config['token']);
+$response = $telegram->setWebhook(
+    'https://'.$url,
+    $certificate
+);
+var_dump([
+    'url' => 'https://'.$url,
+    'response' => $response
+]);
