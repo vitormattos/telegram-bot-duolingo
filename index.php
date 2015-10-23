50ac8b4ec3adfc485f5a60b0f6ad1714c6149ff3
Vitor Mattos <vitor.mattos@phprio.org>
2015-10-23 02:24:46 -0200

    teste

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ use Telegram\Bot\Api;
 require 'vendor/autoload.php';
 require 'config.php';
 
+putenv('DATABASE_URL=http://postgres:postgres@localhost:5432/lupulocalizador');
+header("Content-type:image/png");
+$ranking = new Commands\RankingCommand();
+echo $ranking->handle();
+return;
+
 $telegram = new Api($config['token']);
 
 // Standalone
